dab9328465bf06882d2fd3ca1faa10274d794bd6
KelvinCreighton <kelvincreighton413@gmail.com>
2023-12-21 18:48:26 -0700

    shrunk executor code

--- byte_array_executor.ps1
+++ byte_array_executor.ps1
@@ -1,25 +1,3 @@
-
-$hexString = "6563686f2068656c6c6f20776f726c64"
-
-function Convert-HexStringToByteArray {
-	param (
-    	[string]$hexString
-	)
-    
-	$byteArray = @()
-	for ($i = 0; $i -lt $hexString.Length; $i += 2) {
-    	$byte = [byte]::Parse($hexString.Substring($i, 2), [System.Globalization.NumberStyles]::HexNumber)
-    	$byteArray += $byte
-	}
-    
-	return $byteArray
-}
-
-$byteArray = Convert-HexStringToByteArray $hexString
-
-$command = [System.Text.Encoding]::UTF8.GetString($byteArray)
-
-Invoke-Expression $command
-
+$b=0..15|%{[byte]::Parse("6563686f2068656c6c6f20776f726c64".Substring($_*2,2),'Hex')};Invoke-Expression([System.Text.Encoding]::UTF8.GetString($b))
 
 pause
