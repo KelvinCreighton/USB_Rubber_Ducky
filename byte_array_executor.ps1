4597b823e4c53da045d6ea4a04ab6439f733360a
KelvinCreighton <kelvincreighton413@gmail.com>
2024-01-05 19:40:19 -0700

    started the code for arduino nano

--- byte_array_executor.ps1
+++ byte_array_executor.ps1
@@ -1,3 +1,7 @@
-$b=0..15|%{[byte]::Parse("6563686f2068656c6c6f20776f726c64".Substring($_*2,2),'Hex')};Invoke-Expression([System.Text.Encoding]::UTF8.GetString($b))
+$b=0..36|%{[byte]::Parse("6563686f206161616161616161616161616161616161616161616161616162626262626262".Substring($_*2,2),'Hex')};Invoke-Expression([System.Text.Encoding]::UTF8.GetString($b))
 
 pause
+
+# Execute this in win+r using powershell.exe -Command "<command>"
+
+
